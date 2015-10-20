07f9ed268ac889d0ec8342500233eed8bac20ad8
asd890303 <asd890303@gmail.com>
2015-10-20 19:36:42 +0800

    add spotrepository

---
+++ SiSee_forTest/Models/Repository/SpotRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SiSee_v1.Models.ViewModels;
+
+namespace SiSee_v1.Models.Repository
+{
+    public class SpotRepository
+    {
+        //
+        private sisdbEntities1 db = new sisdbEntities1();
+
+        #region Create
+
+
+        #endregion
+
+        #region Select
+
+        public IEnumerable<Spot> GetAll()
+        {
+            return db.Spot;
+        }
+
+        #endregion
+    }
+}
